a77558c3eda38adb13a563bfd91d6cebd6c70fd2
ldunham9 <35976476+ldunham9@users.noreply.github.com>
2018-02-13 13:39:15 -0700

    FINAL PATCH

--- HighRollers/Program.cs
+++ HighRollers/Program.cs
@@ -16,7 +16,7 @@ namespace HighRollers
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-          
+            Application.Run(new frmHighRollers());
         }
     }
 }
